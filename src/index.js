d396c09c33f3c720b183a0af3bdc9ad091f70ca1
OsipovaNadezda <osipavanadezda@gmail.com>
2022-07-25 22:00:16 +0300

    the functionality on the "our services" block was completed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from './modules/timer';
 import menu from './modules/menu';
 import modal from './modules/modal';
 import pageScroll from './modules/pageScroll';
+import tabs from './modules/tabs';
 
 timer('24 July 2022');
 menu();
 modal();
 pageScroll();
+tabs();
