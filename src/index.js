c5a8935ff00ba6da4a6c1575556b8e65c0530f3d
OsipovaNadezda <osipavanadezda@gmail.com>
2022-07-31 22:25:56 +0300

    the functionality of the calculator is completed

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import modal from './modules/modal';
 import pageScroll from './modules/pageScroll';
 import tabs from './modules/tabs';
 import slider from './modules/slider';
+import calculator from './modules/calculator';
 
-timer('30 July 2022');
+//timer('30 July 2022');
 menu();
 modal();
 pageScroll();
 tabs();
-slider();
+//slider();
+calculator();
